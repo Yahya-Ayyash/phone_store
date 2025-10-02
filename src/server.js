5547e811a5a0cc6a7037841fb6fb38e29d316fbb
Yahya Ayyash <ayyash479@gmail.com>
2025-10-02 14:08:02 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { testConnection } from "./config/db.js";
+import useRouter from "./routes/userRoute.js";
+
+// membuat server
+const app = express();
+const port = 2807;
+
+app.use(express.json());
+ 
+app.use(useRouter);
+
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+  testConnection();
+});
